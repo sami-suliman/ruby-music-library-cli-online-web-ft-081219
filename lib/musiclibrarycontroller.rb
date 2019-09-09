05aec7235a96022a5fa9044b2c02370ee911a139
sami suliman <sami.bco@gmail.com>
2019-09-09 03:30:27 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -64,5 +64,13 @@ class MusicLibraryController
       artist.songs.sort{|a, b| a.name <=> b.name }.each_with_index{|song, index| puts "#{index + 1}. #{song.name} - #{song.genre.name}"}
     end 
   end 
+  
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort{|a, b| a.name <=> b.name }.each_with_index{|song, index| puts "#{index + 1}. #{song.name} - #{song.genre.name}"}
+    end 
+  end 
 
 end 
